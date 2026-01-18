86fa340af3d570128fd0f0f7dde78f9a77fb16f9
zhenya457 <zheka4573@gmail.com>
2026-01-18 19:56:06 +0200

    finalize encrypted env setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/env-test', function () {
+    return [
+        'env' => env('APP_NAME'),
+        'config' => config('app.name'),
+    ];
+});
+
