a22b4a1121cb5b113e218cfe4128ac043d5fa6be
zhenya457 <zheka4573@gmail.com>
2026-01-29 13:22:28 +0200

    project cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MeshGradientController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mesh', MeshGradientController::class);
 
 
 
