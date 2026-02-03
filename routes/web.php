e828b7f6de1b026af5ccd40e7fa039defb77ed21
zhenya457 <zheka4573@gmail.com>
2026-02-03 14:34:10 +0200

    clean up

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\UserPostController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
-
 
 
 
@@ -13,18 +9,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/users', function () {
-        return 'Ok';
-    });
-});
 
-Route::get('/post/{post}', function (Post $post) {
-    return $post;
-});
-
-
-Route::scopeBindings()->group(function ()
-{
-    Route::get('/user/{user}/posts/{post}', [UserPostController::class, 'show'])->name('users.posts.show');
-});
