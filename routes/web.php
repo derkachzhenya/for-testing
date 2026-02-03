0ac1e737031ac0785d44dd0abe8193e5667e694f
zhenya457 <zheka4573@gmail.com>
2026-02-03 17:39:02 +0200

    created controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -10,3 +12,10 @@ Route::get('/', function () {
 });
 
 
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
+Route::get('/category/(category)', [CategoryController::class, 'show'])->name('category.show');
+Route::get('/category/(category)/edit', [CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/(category)', [CategoryController::class, 'put'])->category('category.put');
+Route::delete('/category/(category)', [CategoryController::class, 'delete'])->name('category.delete');
