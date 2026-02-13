cbf3e75ec706148abd18acb6cfb9260ab5564fcf
zhenya457 <zheka4573@gmail.com>
2026-02-13 09:24:54 +0200

    clean up project

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
-Route::get('/category/{category}', [CategoryController::class, 'show'])->name('category.show');
-Route::get('/category/{category}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-Route::put('/category/{category}', [CategoryController::class, 'update'])->name('category.update');
-Route::delete('/category/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
