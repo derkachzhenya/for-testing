f1a5700b4fc6c1de807bec9c0af6ff294fff50eb
zhenya457 <zheka4573@gmail.com>
2026-03-02 21:13:15 +0200

    testing for project

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,16 +8,6 @@ class UserController extends Controller
 {
     public function index(): void
     {
-        // Bad: N+1 problem
-        $users = User::all();
-        foreach ($users as $user) {
-            echo $user->posts->count(); // 1 query per user!
-        }
-
-        // Good: eager loading
-        $users = User::with('posts')->get();
-        foreach ($users as $user) {
-            echo $user->posts->count(); // only 2 queries total
-        }
+      
     }
 }
