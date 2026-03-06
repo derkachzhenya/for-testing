1e658c8302a2a820d817f29f6e924df66fd1281e
zhenya457 <zheka4573@gmail.com>
2026-03-06 18:02:02 +0200

    clean up

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::view('/', 'home.index')->name('home.index');
-
-Route::resource('post', PostController::class);
-Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comment.store');
-
-Route::resource('/task', TaskController::class);
-
-Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
