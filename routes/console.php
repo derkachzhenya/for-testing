a81de1c5072743d79793ebc980ee44d8289ef1ce
zhenya457 <zheka4573@gmail.com>
2026-06-13 10:36:08 +0300

    add scheduled command to sync bookings without overlapping

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,36 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Prevent overlapping scheduled runs
+Schedule::command('bookings:sync')
+    ->everyMinute()
+    ->withoutOverlapping();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
