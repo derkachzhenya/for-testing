47a9a2ae550b5b9851dd44d22381373766a0761e
zhenya457 <zheka4573@gmail.com>
2026-06-16 10:01:23 +0300

    add external provider service to fetch provider status

---
+++ app/Services/ExternalProviderService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class ExternalProviderService
+{
+    public function getProviderStatus(): string
+    {
+        return rescue(
+            fn () => Http::get('provider/status')
+                ->throw()
+                ->json('status'),
+            'unavailable'
+        );
+    }
+}
